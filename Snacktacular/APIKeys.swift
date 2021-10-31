3cd819b7e476d8a3fecdb10df53609a443c2ef00
phillt3 <tracypb@bc.edu>
2021-10-31 12:48:50 -0400

    8.1 through 8.8

---
+++ Snacktacular/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  Snacktacular
+//
+//  Created by Phillip  Tracy on 10/31/21.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyAUKvSjik9dcPtHSnXbWIOpiEOlCWUN1nE"
+}
