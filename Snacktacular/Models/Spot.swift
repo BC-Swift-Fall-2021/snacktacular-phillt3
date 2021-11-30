7bf8762bed04c1f3e7da287d29025cac390e450a
phillt3 <tracypb@bc.edu>
2021-11-30 14:18:32 -0500

    Working with buttons in prep for audio implementation

--- Snacktacular/Models/Spot.swift
+++ Snacktacular/Models/Spot.swift
@@ -21,12 +21,14 @@ class Spot: NSObject, MKAnnotation {
         return ["name": name, "address": address, "latitude": latitude, "longitude": longitude, "averageRating": averageRating, "numberOfReviews": numberOfReviews, "postingUserID": postingUserID]
     }
     
-    var location: CLLocation {
-        return CLLocation(latitude: latitude, longitude: longitude)
-    }
+
     
     var title: String? {
         return name
+    }
+    
+    var location: CLLocation {
+        return CLLocation(latitude: latitude, longitude: longitude)
     }
     
     var latitude: CLLocationDegrees {
